96468eb2ace4361935d9c1ef785389b84842715a
Alessandro Strino <Viuleeenz@users.noreply.github.com>
2022-06-10 18:43:35 +0200

    20220610/forward-improvement
    updating server functionality and README file

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Simple_Server
-Utility that could be used to create a simple HTTP(s) server that log incoming requests for testing purposes (es., Clickjacking, Malware endpoint communication, etc..)
+Utility that could be used to create a simple HTTP(s) server that logs incoming requests for testing purposes (es., Clickjacking, Malware endpoint communication, etc..)
+
+Note. This project aims to help myself with my daily tasks. Because of that, updates and support will be provided when needed.
+
+
+## Updates
+2022/06/10 Server supports forwarding capabilities. More precisely, it's now possible through the switch `-f` to specify an endpoint to forwards **incoming POST request**
--- simple_server.py
+++ simple_server.py
@@ -6,46 +6,59 @@ SSL certificate: openssl req -x509 -newkey rsa:2048 -keyout key.pem -out cert.pe
 
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from sys import argv
+import requests
 import ssl
 import logging
 import argparse
 
+forward = None
+
 
 class SimpleServer(BaseHTTPRequestHandler):
-    def _set_response(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
+	def _set_response(self):
+		self.send_response(200)
+		self.send_header('Content-type', 'text/html')
+		self.end_headers()
 
-    def do_GET(self):
-        logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
-        self._set_response()
-        self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
+	def do_GET(self):
+		logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
+		self._set_response()
+		self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
 
-    def do_POST(self):
-        content_length = int(self.headers['Content-Length'])
-        post_data = self.rfile.read(content_length)
-        logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
-                str(self.path), str(self.headers), post_data.decode('utf-8'))
+	def do_POST(self):
+		content_length = int(self.headers['Content-Length'])
+		post_data = self.rfile.read(content_length)
+		logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
+				str(self.path), str(self.headers), post_data.decode('utf-8'))
+		if forward:
+			forward_request(forward,post_data)
 
-        self._set_response()
-        self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
+		self._set_response()
+		self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
+
+
+def forward_request(forward, data):
+	try:
+		response = requests.post(forward, data, timeout=0.0000000001)
+	except requests.exceptions.ReadTimeout: 
+		pass
+
 
 def run(port, connection, keyfile, certfile, server_class=HTTPServer, handler_class=SimpleServer):
-    logging.basicConfig(level=logging.INFO)
-    server_address = ('', port)
-    httpd = server_class(server_address, handler_class)
+	logging.basicConfig(level=logging.INFO)
+	server_address = ('', port)
+	httpd = server_class(server_address, handler_class)
 
-    if connection == 'https':
-    	httpd.socket = ssl.wrap_socket (httpd.socket, keyfile=keyfile, certfile=certfile, server_side=True)
-    
-    logging.info('httpd Server Started...\n')
-    try:
-        httpd.serve_forever()
-    except KeyboardInterrupt:
-        logging.info('Exception: unable to start httpd server...\n')
-    httpd.server_close()
-    logging.info('Stopping httpd...\n')
+	if connection == 'https':
+		httpd.socket = ssl.wrap_socket (httpd.socket, keyfile=keyfile, certfile=certfile, server_side=True)
+	
+	logging.info('httpd Server Started...\n')
+	try:
+		httpd.serve_forever()
+	except KeyboardInterrupt:
+		logging.info('Exception: unable to start httpd server...\n')
+	httpd.server_close()
+	logging.info('Stopping httpd...\n')
 
 
 if __name__ == '__main__':
@@ -54,10 +67,13 @@ if __name__ == '__main__':
 		For now, it supports GET and POST methods.''',
 		formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 	parser.add_argument("-t", "--type", choices=["http","https"], type=str, help="create a server with http or https capabilities",
-		default="http")
+	default="http")
 	parser.add_argument("-p", "--port", type=int, help="define server port", default=4444)
 	parser.add_argument("-k", "--key", help="path to keyfile (required for SSL)", default='./key.pem')
 	parser.add_argument("-c", "--cert", help="path to certificate file (required for SSL)", default='./cert.pem')
+	parser.add_argument("-f", "--forward", help="forward incoming POST request toward a defined endpoint. \
+	Define endpoint following the format => proto:ip:port (e.g., http://127.0.0.1:8000/ )", default=None)
 	
 	args = parser.parse_args()
+	forward = args.forward
 	run(port=args.port,connection=args.type, keyfile=args.key, certfile=args.cert)
